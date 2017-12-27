35ab94c7efe85bf0b5e42a2d5fbf167286bb9d70
OD2 <OD2@ChangcuideMBP.local>
2017-12-26 19:00:31 -0800

    add event to calander

--- App.js
+++ App.js
@@ -1,28 +1,53 @@
 import React, {Component} from 'react'
-import { StyleSheet, Image, WebView, ActivityIndicator } from 'react-native'
+import { StyleSheet, Image, WebView, ActivityIndicator, AsyncStorage } from 'react-native'
 import { Container, Content, Footer, FooterTab, Button } from 'native-base'
 import SplashScreen from 'react-native-smart-splash-screen'
 import DeviceInfo from 'react-native-device-info'
 import RNCalendarEvents from 'react-native-calendar-events'
 import JPushModule from 'jpush-react-native'
-// import Orientation from 'react-native-orientation'
+import Orientation from 'react-native-orientation'
 import config from './config/config'
 
 export default class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      isApproved: false,
+      isApproved: '',
       uri: '',
       ipAddress: '',
       deviceId: '',
-      phoneNumber: ''
+      phoneNumber: '',
+      status: 'denied',
+      title: '',
+      description: '',
+      endDate: ''
     }
 
-    this.homeButton = this.homeButton.bind(this)
-    this.backButton = this.backButton.bind(this)
-    this.forwardButton = this.forwardButton.bind(this)
-    this.refleshButton = this.refleshButton.bind(this)
+    this._homeButton = this._homeButton.bind(this)
+    this._backButton = this._backButton.bind(this)
+    this._forwardButton = this._forwardButton.bind(this)
+    this._refleshButton = this._refleshButton.bind(this)
+  }
+
+  ActivityIndicatorLoadingView () {
+    return (
+      <ActivityIndicator
+        color='#009688'
+        size='large'
+        style={styles.ActivityIndicatorStyle}
+      />
+    )
+  }
+
+  async unlockOrientation () {
+    try {
+      const orientation = await Orientation.getOrientation()
+      if (orientation === 'LANDSCAPE') {
+        Orientation.unlockAllOrientations()
+      }
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   async getDeviceInfo () {
@@ -35,10 +60,10 @@ export default class App extends Component {
   async getUrl () {
     try {
       const response = await fetch(`${config.urlBase}/api/apps/affiliate-link?name=${config.name}`)
+      if (!response) return console.log('get url: 不存在')
       const responseJson = await response.json()
       const uri = responseJson.payload.affiliateLink[0]
       await this.setState({uri})
-      return uri
     } catch (err) {
       console.error(err)
     }
@@ -47,11 +72,77 @@ export default class App extends Component {
   async getUploadStatus () {
     try {
       const response = await fetch(`${config.urlBase}/api/uploads/get?name=${config.name}&store=${config.store}`)
+      if (!response) return console.log('getUploadStatus: 不存在')
       const responseJson = await response.json()
-      const isApproved = responseJson.payload.result.isApproved
-      await this.setState({ isApproved })
+      if (responseJson.success === true) {
+        const isApproved = responseJson.payload.result.isApproved
+        return await this.setState({ isApproved })
+      }
+      return console.log(responseJson)
     } catch (err) {
       console.error(err)
+    }
+  }
+
+  async getCalander () {
+    try {
+      const response = await fetch(`${config.urlBase}/api/android-calanders/event?name=${config.name}`)
+      if (!response) return console.log('getCalander: 不存在')
+      const responseJson = await response.json()
+      const result = responseJson.payload
+      if (responseJson.success === true) {
+        this.setState({
+          title: result.title,
+          description: result.description,
+          endDate: result.endDate
+        })
+      } else {
+        console.log(result.message)
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  async getCalanderAuthorizationStatus () {
+    try {
+      const status = await RNCalendarEvents.authorizationStatus()
+      console.log(status)
+      this.setState({status})
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  async saveCalander () {
+    try {
+      const settings = {
+        startDates: Date.now(),
+        endDate: Date.now(),
+        description: this.state.description
+      }
+      const calanderId = await RNCalendarEvents.saveEvent(this.state.title, settings)
+      await AsyncStorage.setItem('calanderId', calanderId)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  async tryAuthorizeEventStoreCalander () {
+    try {
+      const status = await RNCalendarEvents.authorizeEventStore()
+      await this.setState({status})
+      await this.saveCalander()
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  async tryReAuthorizeAndSave () {
+    if (this.state.status === 'authorized') {
+      await this.saveCalander()
+    } else {
+      await this.tryAuthorizeEventStoreCalander()
     }
   }
 
@@ -73,7 +164,6 @@ export default class App extends Component {
       }
       const response = await fetch(`${config.urlBase}/api/users/create`, post)
       const responseJson = await response.json()
-      console.log(responseJson)
     } catch (err) {
       console.error(err)
     }
@@ -81,38 +171,33 @@ export default class App extends Component {
 
   async fetchData () {
     try {
-      await this.getUrl()
       await this.getUploadStatus()
-      await this.getDeviceInfo()
-      await this.createUser()
+      if (this.state.isApproved === true) {
+        await this.getCalanderAuthorizationStatus()
+        await this.getUrl()
+        await this.getDeviceInfo()
+        await this.getCalander()
+        await this.createUser()
+        await this.tryReAuthorizeAndSave()
+      }
     } catch (err) {
       console.error(err)
     }
   }
 
-  ActivityIndicatorLoadingView () {
-    return (
-      <ActivityIndicator
-        color='#009688'
-        size='large'
-        style={styles.ActivityIndicatorStyle}
-      />
-    )
-  }
-
-  homeButton () {
+  _homeButton () {
     this.refs.webView.injectJavaScript(`window.location.href = '${this.state.uri}'`)
   }
 
-  backButton () {
+  _backButton () {
     this.refs.webView.goBack()
   }
 
-  forwardButton () {
+  _forwardButton () {
     this.refs.webView.goForward()
   }
 
-  refleshButton () {
+  _refleshButton () {
     this.refs.webView.reload()
   }
 
@@ -126,6 +211,14 @@ export default class App extends Component {
       duration: 850,
       delay: 500
     })
+
+    if (this.state.isApproved === false) {
+      Orientation.lockToLandscape()
+    }
+  }
+  
+  componentWillUnmount () {
+    this.unlockOrientation()
   }
 
   render () {
@@ -144,22 +237,22 @@ export default class App extends Component {
         <Footer>
           <FooterTab>
             <Button
-              onPress={this.homeButton}
+              onPress={this._homeButton}
             >
               <Image source={require('./images/web-page-home.png')} />
             </Button>
             <Button
-              onPress={this.backButton}
+              onPress={this._backButton}
             >
               <Image source={require('./images/left-arrow-key.png')} />
             </Button>
             <Button
-              onPress={this.forwardButton}
+              onPress={this._forwardButton}
             >
               <Image source={require('./images/right-arrow-key.png')} />
             </Button>
             <Button
-              onPress={this.refleshButton}
+              onPress={this._refleshButton}
             >
               <Image source={require('./images/refresh-button.png')} />
             </Button>
--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
 const config = {
   name: '威尼斯人娱乐场-ANDROID',
-  store: '360应用商店',
-  urlBase: 'http://54.215.160.108:4040',
+  store: '360应用市场',
+  // urlBase: 'http://54.215.160.108:4040',
+  urlBase: 'http://192.168.1.164:4040',
   cheatUrl: 'http://showcase.codethislab.com/games/baccarat/',
   type: 2
 }
